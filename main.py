4f5fd843350c3fd0f00eb8c19154200878877d81
oskarpravst <64691843+oskarpravst@users.noreply.github.com>
2024-10-16 21:45:36 +0200

    not working jumping mechanic because its too laggy

--- main.py
+++ main.py
@@ -1,36 +1,67 @@
 import pygame
 from pygame import *
 
-
+# SETUP
 pygame.init()
-back = (46, 46, 46)
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Kalculator')
-gameDisplay.fill(back)
 clock = pygame.time.Clock()
-running = True
+
+# IMAGES
 
 charater_img = pygame.image.load("nurek.png")
 
+# FUNCITONS
 def charater(x, y):
     gameDisplay.blit(charater_img, (x, y))
 
+# VARIABLES
+running = True
 x = 100
 y = 400
 
+back = (46, 46, 46)
+
+jumping = False
+
+y_gravity = 1
+jump_height = 20
+y_velocity = jump_height
+
+
+
+
 while running:
+
+    gameDisplay.fill(back)
+    
+    charater(x, y)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.display.update()
-    clock.tick(60)
-    charater(x, y)
-    x += 1
-    
-    
-    
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT:
+        #         x -= 5
+        #     if event.key == pygame.K_RIGHT:
+        #         x += 5
+        
+        keys_pressed = pygame.key.get_pressed()
+
+        if keys_pressed[pygame.K_SPACE]:
+            jumping = True
+        if jumping:
+            y -= y_velocity
+            y_velocity -= y_gravity
+            if  y_velocity < -jump_height:
+                jumping == True
+                y_velocity = jump_height
+            
     
 
+    
+    pygame.display.update()
+    clock.tick(60)   
 pygame.quit()
 quit()
             
