8d83a81085a95567a712ef14f3960d1c98073d83
oskarpravst <64691843+oskarpravst@users.noreply.github.com>
2024-10-22 20:37:26 +0200

    adding jumping actually

--- main.py
+++ main.py
@@ -3,65 +3,56 @@ from pygame import *
 
 # SETUP
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Kalculator')
 clock = pygame.time.Clock()
 
 # IMAGES
+character_img = pygame.image.load("nurek.png")
 
-charater_img = pygame.image.load("nurek.png")
-
-# FUNCITONS
-def charater(x, y):
-    gameDisplay.blit(charater_img, (x, y))
+# FUNCTIONS
+def character(x, y):
+    gameDisplay.blit(character_img, (x, y))
 
 # VARIABLES
 running = True
 x = 100
 y = 400
+ground_y = 400
 
 back = (46, 46, 46)
 
 jumping = False
-
-y_gravity = 1
-jump_height = 20
-y_velocity = jump_height
-
-
-
+velocity = 0
+jump_strength = 15  # Set a fixed jump strength
 
 while running:
-
     gameDisplay.fill(back)
     
-    charater(x, y)
+    character(x, y)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_LEFT:
-        #         x -= 5
-        #     if event.key == pygame.K_RIGHT:
-        #         x += 5
         
-        keys_pressed = pygame.key.get_pressed()
-
+    keys_pressed = pygame.key.get_pressed()
+        
+    if not jumping:
         if keys_pressed[pygame.K_SPACE]:
             jumping = True
-        if jumping:
-            y -= y_velocity
-            y_velocity -= y_gravity
-            if  y_velocity < -jump_height:
-                jumping == True
-                y_velocity = jump_height
-            
+            velocity = jump_strength  # Set the initial jump velocity
     
+    if jumping:
+        y -= velocity  # Move the character up
+        velocity -= 1  # Decrease the velocity
 
-    
+        # If the character has fallen back to the ground
+        if y >= ground_y:
+            y = ground_y  # Reset to ground level
+            jumping = False  # Reset jumping state
+            velocity = 0  # Reset velocity
+
     pygame.display.update()
-    clock.tick(60)   
+    clock.tick(60)
+
 pygame.quit()
-quit()
-            
