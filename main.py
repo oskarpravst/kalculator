57ed718b4e1f968375450c716b7400e0664ae6fd
oskarpravst <64691843+oskarpravst@users.noreply.github.com>
2024-10-27 11:02:19 +0100

    adding collision and floor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pygame import *
 
 # SETUP
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+gameDisplay = pygame.display.set_mode((1000, 300))
 pygame.display.set_caption('Kalculator')
 clock = pygame.time.Clock()
 
@@ -18,19 +18,29 @@ def character(x, y):
 
 def spike(x1, y1):
     gameDisplay.blit(spike_img, (x1, y1))
+    gameDisplay.blit(spike_img, (x1+30, y1))
 
 def floor(x2, y2):
     gameDisplay.blit(floor_img, (x2, y2))
 
+        
+
+# MASKS
+character_mask = pygame.mask.from_surface(character_img)
+mask_img = character_mask.to_surface()
+
+spike_mask = pygame.mask.from_surface(spike_img)
+
+
 # VARIABLES
 running = True
 x = 100
-y = 400
-x1 = 800
-y1 = 390
-y2 = 0
-x2 = 50
-ground_y = 400
+y = 150
+x1 = 1000
+y1 = 140
+y2 = 172
+x2 = 1
+ground_y = 150
 
 back = (46, 46, 46)
 
@@ -40,14 +50,21 @@ jump_strength = 10 # Set a fixed jump strength
 
 while running:
     gameDisplay.fill(back)
-    
     floor(x2, y2)
     character(x, y)
-    spike(x1, y1)
     
-    x1 -= 5
+    spike(x1, y1)
+    # gameDisplay.blit(mask_img, (100, 150))
+
+    if character_mask.overlap(spike_mask, (x1 - x, y1 - y)): # subtract coordinates of the spike and the charater
+        print("dead")
+        
+    
+    
+    
+    x1 -= 7
     if x1 < 0:
-        x1 = 800
+        x1 = 1000
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
