ca919b14e2ae87579ef85e74717afd8c005da6c3
oskarpravst <64691843+oskarpravst@users.noreply.github.com>
2024-11-07 17:45:31 +0100

    display score when player dies

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def character(charater_x, charater_y):
 def update_spikes(spike_x_list):
     for i in range(len(spike_x_list)):
         print(spike_x_list[i])
-        spike_x_list[i] -= 10
+        spike_x_list[i] -= spike_speed
         
 
 
@@ -69,10 +69,10 @@ floor_x = 0
 ground_y = 150
 floor_level = 0
 random_spawn_spike = random.randint(0, 2)
-spike_speed = random.randint(6, 12)
-death_text = text("YOU DIED", 500, 100, 50)
+spike_speed = 10
+death_text = text("YOU DIED, TOTAL SCORE: ", 500, 100, 50)
 score = 0
-score_text = text(str(score), 500, 100, 50)
+score_text = text(str(score), 500, 60, 50)
 back = (46, 46, 46)
 jumping = False
 velocity = 0
@@ -89,7 +89,7 @@ while running:
         
         spike_x_list.append(1000)
         timer = 0
-        sigma = random.randint(18, 50)
+        sigma = random.randint(15, 40)
     update_spikes(spike_x_list)
 
     gameDisplay.fill(back)
@@ -97,19 +97,26 @@ while running:
     character(charater_x, charater_y)
     
     spike(spike_x_list)
-   
+    if score == 10:
+        spike_speed = 11
+    if score == 20:
+        spike_speed = 12
+    if score == 30:
+        spike_speed = 14
     
     # gameDisplay.blit(mask_img, (100, 150))
 
     for i in range(len(spike_x_list)):
         if character_mask.overlap(spike_mask, (spike_x_list[i] - charater_x, 140 - charater_y)): # subtract coordinates of the spike and the charater
             gameDisplay.blit(death_text[0], death_text[1])
+            score_text = text(str(score), 900, 100, 50)
+            gameDisplay.blit(score_text[0], score_text[1])
             pygame.display.update()
             pygame.time.delay(2000)
             running = False
     
     for i in range(len(spike_x_list)):
-        if spike_x_list[i] == 0:
+        if -11<spike_x_list[i]<0 :
             score += 1
             score_text = text(str(score), 500, 100, 50)
             
